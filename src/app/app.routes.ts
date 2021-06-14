ac82af490eca7b1db0c874caab689b0f2316f2e1
Rodrigo dos Santos <rodrigo.contato@live.com>
2021-06-14 20:50:58 -0300

    Commit_1

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { Routes } from "@angular/router";
+
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+export const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'restaurants', component: RestaurantsComponent }
+]
---
+++ src/app/restaurants/restaurant.service.ts
@@ -0,0 +1,18 @@
+import { MEAT_API } from './../app.api';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Restaurant } from './restaurant/restaurant.model';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+
+@Injectable()
+
+export class RestaurantsService {
+
+    constructor(private http: Http){}
+
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+          .map(response => response.json())
+    }
+}
