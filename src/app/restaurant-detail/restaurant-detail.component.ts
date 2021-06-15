d617288d4f2a7c881620ed4f0f3eeaa664be7b2c
Rodrigo dos Santos <rodrigo.contato@live.com>
2021-06-15 13:08:19 -0300

    commit-15/06

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,24 @@
+import { Restaurant } from './../restaurants/restaurant/restaurant.model';
+import { RestaurantsService } from './../restaurants/restaurant.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+
+  restaurant: Restaurant[]
+
+  constructor(
+    private restaurantService: RestaurantsService,
+    private route: ActivatedRoute
+    ) { }
+
+  ngOnInit() {
+    this.restaurantService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant)
+  }
+
+}
--- src/app/restaurants/restaurant.service.ts
+++ src/app/restaurants/restaurant.service.ts
@@ -1,9 +1,14 @@
-import { MEAT_API } from './../app.api';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { Restaurant } from './restaurant/restaurant.model';
+
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+
+import { Restaurant } from './restaurant/restaurant.model';
+import { MEAT_API } from './../app.api';
+import { ErrorHandler } from 'app/app.error-handler';
+
 
 @Injectable()
 
@@ -14,5 +19,14 @@ export class RestaurantsService {
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
           .map(response => response.json())
+          .catch(ErrorHandler.handleError)
     }
+
+    restaurantById(id: string): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+          .map(response => response.json())
+          .catch(ErrorHandler.handleError)
+    }
+
+
 }
